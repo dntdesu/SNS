1c31656bb427a7c993fa7405a92c092f954bd389
dntdesu <38828816+dntdesu@users.noreply.github.com>
2019-09-30 18:27:56 +0900

    Create First setup activity

--- app/src/main/java/jp/ac/ecc/sk3a17/sns/MainActivity.java
+++ app/src/main/java/jp/ac/ecc/sk3a17/sns/MainActivity.java
@@ -1,26 +1,46 @@
 package jp.ac.ecc.sk3a17.sns;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
-
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private NavigationView navigationView;
+    public static final String TAG = MainActivity.class.getSimpleName();
+
     private DrawerLayout drawerLayout;
-    private RecyclerView postList;
+    private NavigationView navigationView;
     private Toolbar toolbar;
-    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private ActionBarDrawerToggle actionBarDrawerToggle; //for opening navigation view when tap on icon on the appbar
+    private FirebaseAuth mAuth; //for authentication
+    private DatabaseReference userRef; //for connecting to real time database
+    private GoogleSignInClient mGoogleSignInClient;
+    private CircleImageView navProfileImage;
+    private String currentUserID = null;
 
 
     @Override
@@ -28,18 +48,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        drawerLayout = findViewById(R.id.drawable_layout);
-        navigationView = findViewById(R.id.navigation_view);
-        toolbar = findViewById(R.id.main_page_layout);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close);
+        Log.e(TAG, "Start Main");
+        setContentView(R.layout.activity_main);
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        navigationView = findViewById(R.id.navigation_view);
+        drawerLayout = findViewById(R.id.drawable_layout);
+
+        //Create a node named Users on database
+        userRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+
+        View navView = navigationView.inflateHeaderView(R.layout.navigation_header); //set header for navigation view
+        navProfileImage = navView.findViewById(R.id.nav_header_image);
+
+        //custom toolbar and ActionBarDrawerToggle's things
+        toolbar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(toolbar);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, R.string.drawer_open, R.string.drawer_close);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
 
-        View navView = navigationView.inflateHeaderView(R.layout.navigation_header);
+        //display profile image on navigation
+        userRef.child(currentUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists() && dataSnapshot.hasChild("Profile Images")) {
+                    String image = dataSnapshot.child("Profile Images").getValue().toString();
+                    Picasso.get().load(image).into(navProfileImage);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
@@ -47,6 +101,51 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    //It will be run after OnCreate, check if user has login or not
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            SendToLogin();
+        } else {
+            //check user is on database or not
+            CheckUserExistence();
+        }
+    }
+
+    private void CheckUserExistence() {
+        final String current_user_id = mAuth.getCurrentUser().getUid();
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (!dataSnapshot.hasChild(current_user_id)) {
+                    SendToSetUp();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void SendToSetUp() {
+        Intent setUpIntent = new Intent(MainActivity.this, SetUpActivity.class);
+        setUpIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(setUpIntent);
+        finish();
+    }
+
+    private void SendToLogin() {
+        Intent loginIntent = new Intent(MainActivity.this, LogInActivity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(loginIntent);
+        finish();
     }
 
     @Override
